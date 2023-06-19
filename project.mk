f3a2d1178c12b01a55d90237c9d70ea7d6f48617
LargeBee <rowant@firemail.cc>
2023-06-19 17:44:01 +0100

    Setup boilerplate and added breakout code

--- project.mk
+++ project.mk
@@ -10,10 +10,10 @@ PADVALUE := 0xFF
 VERSION := 0
 
 # 4-ASCII letter game ID
-GAMEID := BOIL
+GAMEID := MONS
 
 # Game title, up to 11 ASCII chars
-TITLE := BOILERPLATE
+TITLE := MONSTER
 
 # New licensee, 2 ASCII chars
 # Homebrew games FTW!
@@ -34,7 +34,7 @@ MBC := 0x00
 SRAMSIZE := 0x00
 
 # ROM name
-ROMNAME := boilerplate
+ROMNAME := monster
 ROMEXT  := gb
 
 
