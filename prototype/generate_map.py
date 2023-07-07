48f010c08e0e5cfe4eddaa7e3d3c33f7c8ef9ba8
LargeBee <rowant@firemail.cc>
2023-07-07 13:24:15 +0100

    added ability to save map string

--- prototype/generate_map.py
+++ prototype/generate_map.py
@@ -1,6 +1,9 @@
+#Imports
+from datetime import datetime
+
 #Base Features
-width = 15
-height = 15
+width = 60
+height = 60
 features = {'floor': '.', 'wall': '#', 'entrance': 'S', 'exit': 'F'}
 algos = {'block': 0, 'cell': 1}
 
@@ -14,9 +17,22 @@ algos = {'block': 0, 'cell': 1}
 algo = algos['cell']
 
 command = ''
+game_map = ''
+
+def save_map_to_file():
+    current_datetime = str(datetime.now().strftime("%Y-%m-%d %H-%M-%S"))
+    file_name = f'{current_datetime}.txt'
+    with open(file_name, 'w') as f:
+        for y in range(height):
+            for x in range(width):
+                f.write(game_map[(width * y) + x])
+            f.write('\n')
 
 while not command == 'q':
-    #Generate and display map
+    #Make map from 0s and 1s
+    #For every 0 put floor and every 1 put wall (or vice versa)
+    #Find a start spot
     print('What would you like to do?')
-    command = input('(g)enerate new map, (q)uit, (s)ave: ')
-    
+    command = input('(g)enerate new map, (s)ave and generate new map, (q)uit: ')
+    if command == 's':
+        save_map_to_file()
